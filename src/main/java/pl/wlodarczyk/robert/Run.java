0013d2dc656b5d2dcbb27ca56170f7b504e5dc26
Robert_Wlodarczyk <robert.michal.wlodarczyk@gmail.com>
2021-01-02 14:48:47 +0100

    Initial commit

---
+++ src/main/java/pl/wlodarczyk/robert/Run.java
@@ -0,0 +1,7 @@
+package pl.wlodarczyk.robert;
+
+public class Run {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/pl/wlodarczyk/robert/main/Game.java
@@ -0,0 +1,35 @@
+package pl.wlodarczyk.robert.main;
+
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable {
+
+    public static final int WIDTH = 0;
+    public static final int HEIGHT = 0;
+
+    public static void main(String[] args) {
+
+    }
+
+    public synchronized void start() {
+
+    }
+
+    public void init() {
+
+    }
+
+    public void tick() {
+
+    }
+
+    public void render() {
+
+    }
+
+    @Override
+    public void run() {
+
+
+    }
+}
